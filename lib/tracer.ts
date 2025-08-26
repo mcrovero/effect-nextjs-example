2a2453e6d38b05bf59fa5246556a550122389a66
Mattia Crovero <mcrovero@gmail.com>
2025-08-26 14:15:11 +0200

    feat: improved tracer

--- lib/tracer.ts
+++ lib/tracer.ts
@@ -1,44 +1,59 @@
 import { trace, context as otelContext } from "@opentelemetry/api";
-import { Effect, Tracer, Option, Layer, pipe } from "effect";
+import { Effect, Tracer, Option, Layer, Context } from "effect";
+import { Tracer as OtelTracer, Resource } from "@effect/opentelemetry";
 
-export const makeTracer: Effect.Effect<Tracer.Tracer, never, never> =
+// !! This implementation still has limitations: follow: https://github.com/Effect-TS/effect/pull/5433
+
+const childTracer = Layer.unwrapEffect(
   Effect.gen(function* () {
     const currentTracer = yield* Effect.tracer;
-    return Tracer.make({
-      span(name, parent, context, links, startTime, kind) {
-        const active = trace.getSpan(otelContext.active());
-        const otelParent = active ? active.spanContext() : undefined;
+    return Layer.setTracer(
+      Tracer.make({
+        span(name, parent, context, links, startTime, kind) {
+          const effectiveParent =
+            parent._tag === "Some" ? parent : getOtelParent(context);
 
-        const externalParent = otelParent
-          ? Option.some(
-              Tracer.externalSpan({
-                spanId: otelParent.spanId,
-                traceId: otelParent.traceId,
-                sampled: (otelParent.traceFlags & 1) === 1,
-                context,
-              })
-            )
-          : Option.none();
+          return currentTracer.span(
+            name,
+            effectiveParent,
+            context,
+            links,
+            startTime,
+            kind
+          );
+        },
+        context: currentTracer.context,
+      })
+    );
+  })
+);
 
-        const effectiveParent =
-          parent._tag === "Some" ? parent : externalParent;
+const getOtelParent = (context: Context.Context<never>) => {
+  const active = trace.getSpan(otelContext.active());
+  const otelParent = active ? active.spanContext() : undefined;
+  return otelParent
+    ? Option.some(
+      Tracer.externalSpan({
+        spanId: otelParent.spanId,
+        traceId: otelParent.traceId,
+        sampled: (otelParent.traceFlags & 1) === 1,
+        context,
+      })
+    )
+    : Option.none();
+}
 
-        const span = currentTracer.span(
-          name,
-          effectiveParent,
-          context,
-          links,
-          startTime,
-          kind
-        );
-        return span;
-      },
-      context: currentTracer.context,
-    });
-  });
-
-export const layerTracer: Layer.Layer<never, never, never> = pipe(
-  makeTracer,
-  Effect.map(Layer.setTracer),
-  Layer.unwrapEffect
+export const layerTracer = childTracer.pipe(
+  Layer.provide(OtelTracer.layerGlobal),
+  Layer.provide(
+    Layer.unwrapEffect(
+      Effect.gen(function* () {
+        const resource = yield* Effect.serviceOption(Resource.Resource);
+        if (Option.isSome(resource)) {
+          return Layer.succeed(Resource.Resource, resource.value);
+        }
+        return Resource.layerFromEnv();
+      })
+    )
+  )
 );
